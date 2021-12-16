75ec0ad3830c3b64b50bf9d5b68d77d05796306d
Saydullo <sadi.abdulazizov.2001@gmail.com>
2021-12-16 16:03:45 +0500

    Initial Commit

---
+++ MyTaxsiProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTaxsiProject
+//
+//  Created by 1 on 16/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
