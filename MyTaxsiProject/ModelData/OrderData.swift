add2bf04095fc4d6902eab0cf7a404bfbe3dee1e
Saydullo <sadi.abdulazizov.2001@gmail.com>
2021-12-17 17:36:14 +0500

    pods file

---
+++ MyTaxsiProject/ModelData/OrderData.swift
@@ -0,0 +1,29 @@
+//
+//  OrderData.swift
+//  MyTaxsiProject
+//
+//  Created by 1 on 17/12/21.
+//
+
+import UIKit
+
+struct OrderData {
+    let dayLabel             : String
+    let characterRedImage    : UIImage
+    let streetTashkenLabel   : String
+    let characterBlueImage   : UIImage
+    let streetXodjaevaLabel  : String
+    let timeCarLabel         : String
+    let carImageView         : UIImage
+
+}
+struct OrderDataModel {
+    static let data: [OrderData] = [
+        OrderData(dayLabel: "6 Июля, Вторник", characterRedImage: UIImage(named: ""), streetTashkenLabel: <#T##String#>, characterBlueImage: <#T##UIImage#>, streetXodjaevaLabel: <#T##String#>, timeCarLabel: <#T##String#>, carImageView: <#T##UIImage#>)
+    
+    
+    
+    
+    
+    ]
+}
